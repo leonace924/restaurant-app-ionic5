142f519f0de0f0e3904a16aea371b9cd7f409271
Leon924 <meobius123@gmail.com>
2020-04-11 00:21:57 +0800

    migrate payment-checkout page

--- src/app/pages/payment-checkout/payment-checkout.page.ts
+++ src/app/pages/payment-checkout/payment-checkout.page.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-payment-checkout',
+  selector: 'page-payment-checkout',
   templateUrl: './payment-checkout.page.html',
   styleUrls: ['./payment-checkout.page.scss'],
 })
 export class PaymentCheckoutPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public navCtrl: NavController,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  GoMenu() {
+    this.router.navigateByUrl('/qr-scanner');
   }
 
 }
