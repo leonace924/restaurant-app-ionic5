a60a83038a07c054773b9186c1da12383745431d
Leon924 <meobius123@gmail.com>
2020-04-11 00:29:51 +0800

    migrate payment-options page

--- src/app/pages/payment-options/payment-options.page.ts
+++ src/app/pages/payment-options/payment-options.page.ts
@@ -1,15 +1,84 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController, ToastController, ModalController, LoadingController } from '@ionic/angular';
+import { OrdersService } from '../../services/orders/orders.service';
+import { MenuRestaurantService } from '../../services/menurestaurant/menurestaurant.service';
 
 @Component({
-  selector: 'app-payment-options',
+  selector: 'page-payment-options',
   templateUrl: './payment-options.page.html',
   styleUrls: ['./payment-options.page.scss'],
 })
 export class PaymentOptionsPage implements OnInit {
 
-  constructor() { }
+  orders: any;
+  sitId: any;
+  loader: any = null;
+
+  constructor(
+    public navCtrl: NavController,
+    private ordersProv: OrdersService, 
+    private toastCtrl: ToastController,
+    public viewCtrl: ModalController, 
+    private Menu: MenuRestaurantService, 
+    public loadingCtrl: LoadingController,
+    public router: Router
+  ) {
+    // Leon check this after API
+    // this.orders = this.navParams.get('orders');
+    // this.sitId = this.navParams.get('sitId');
+  }
 
   ngOnInit() {
   }
 
+  async goToCheckout() {
+
+    let loader = await this.loadingCtrl.create({
+      message: "requesting the waiter",
+    });
+
+    await loader.present();
+
+    this.Menu.CallWaiter(this.sitId).then(() => {
+      loader.dismiss();
+      this.router.navigateByUrl('/payment-checkout');
+    }).catch(err => {
+      loader.dismiss();
+      this.presentToast("Este Restaurante no posee Mozos");    
+    });
+    
+  }
+  
+
+  MercadoPago() {
+    this.loader = true;
+    this.ordersProv.MercadoPagoPayment(this.orders)
+      .then(data => {
+        this.loader = false;
+        this.router.navigateByUrl('/payment-checkout');
+      })
+      .catch(error => {
+        this.loader = false;
+        this.presentToast(error.detail);
+      })
+  }
+
+  async presentToast(text) {
+    let toast = await this.toastCtrl.create({
+      message: text,
+      position: 'bottom',
+      buttons: [
+        {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+
+    await toast.present();
+  }
 }
