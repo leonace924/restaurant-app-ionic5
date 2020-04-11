b3a81d59f2b63ccd5c84612afdeaa008dc66a7bc
Leon924 <meobius123@gmail.com>
2020-04-12 00:34:58 +0800

    update package.json and config files

--- src/app/config/url.service.ts
+++ src/app/config/url.service.ts
@@ -1,2 +1,2 @@
-export const URL_SERVICE = "";
-export const PREFIXE = "";
+export const URL_SERVICE = "http://ec2-3-130-111-166.us-east-2.compute.amazonaws.com/api";
+export const PREFIXE = "Restaurant";
